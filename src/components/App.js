a89642f04aacd46b837eb6976c17b251b51a09e7
Natale Toscano <kickalliance195@gmail.com>
2021-06-21 23:25:48 -0400

    Fully fix bugs; get game to working order; clean up code in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,70 +1,76 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import '../App.css';
 import Notification from './Notification';
 import GameBoard from './GameBoard';
 import Scoreboard from './Scoreboard';
 
 function App() {
-  const [state, setState] = useState({ text: '', numCards: 4, notificationMilliseconds: 2500, clickedCards: [] });
+  const [notificationText, setNotificationText] = useState('');
+  const [numCards, setNumCards] = useState();
+  const correctCards = useRef();
   const isFading = useRef(null);
-  const notificationMilliseconds = 2500;
+  const notificationMilliseconds = 5000;
+
+  const restart = () => {
+    // could possibly be called start
+    // but i guess in one form or another
+    // it is restarting because it's been
+    // started once before in this world
+    setNumCards(4);
+    correctCards.current = [];
+  };
+  useEffect(() => restart(), []); // restart (start?) at start (restart?)
 
   const lose = () => {
     // lose the game. it restarts??
-    setState(prev => { prev.notificationMilliseconds = 10000000; return prev; });
-    setImmediate(() => setTextThenDelete('You lost the game. It has restarted.'));
-    setState(prev => { prev.numCards = 4; prev.clickedCards = []; return prev; })
-  }
+    // but not before letting the player know they lost
+    setTextThenClear(
+      'You lost the game. It has restarted. Try to beat your high score!'
+    );
+    restart();
+  };
 
-  const win = () => {
-    console.log('win called');
+  const levelUp = () => {
     // win the game. it levels up??
-    //updateState({ notificationMilliseconds: 10000000 });
-    setState(state => {
-      state.notificationMilliseconds = 10000000;
-      state.numCards += 2;
-      state.clickedCards = [];
-      return state;
-    });
-    setImmediate(() => setTextThenDelete('You won this level. You have moved to the next level.'));
-  }
+    setNumCards((numCards) => numCards + 2); // level up
+    correctCards.current = []; // restart the 'game'
+    setTextThenClear('You won this level. You have moved to the next level.');
+    // notify player
+  };
 
-  const setTextThenDelete = (text) => {
-    const deleteText = () => {
-      isFading.current = null;
-      setState(prev => { prev.text = ''; return prev; });
-    };
+  const clearText = () => {
+    isFading.current = null;
+    setNotificationText('');
+  };
 
+  const setTextThenClear = (text) => {
     if (isFading.current) clearTimeout(isFading.current);
-    setState(prev => { prev.text = ''; return prev; });
-    setImmediate(setState(prev => { prev.text = ''; return prev; }));//(updateState.bind(this, { text }));
-    isFading.current = setTimeout(deleteText, notificationMilliseconds);
-  }
+    setNotificationText('');
+    setImmediate(setNotificationText.bind(this, text));
+    isFading.current = setTimeout(clearText, notificationMilliseconds);
+  };
 
   const onCardClicked = (id) => {
-    //updateState({ notificationMilliseconds: 2500 });
-    setState(prev => { prev.notificationMilliseconds = 2500; return prev; });
-    setImmediate(() => setTextThenDelete(id + ' was clicked. You didn\'t lose the game, yet!'));
-    if (state.clickedCards.includes(id)) lose();
-    console.log(state.clickedCards, state.numCards);
-    if (state.clickedCards.length + 1 === state.numCards) {
-      win();
-    } else {
-      setState(prevState => {
-        console.log(prevState);
-        prevState.clickedCards = [...prevState.clickedCards, id]
-        return prevState;
-      })
-      //setClickedCards(clickedCards => [...clickedCards, id]);
-    }
-  }
+    setTextThenClear(id + " was clicked. You didn't lose the game, yet!");
 
-  console.log(state.clickedCards);
-  return <div className="app">
-    <Notification time={state.notificationMilliseconds} message={state.text} />
-    <GameBoard cardClicked={onCardClicked.bind(this)} numCards={state.numCards} />
-    <Scoreboard score={10} hiScore={15} header={'Memory Card'} />
-  </div>;
+    if (correctCards.current.includes(id)) lose();
+    if (correctCards.current.length + 1 === numCards) {
+      levelUp();
+    } else {
+      correctCards.current = [...correctCards.current, id];
+    }
+  };
+
+  return (
+    <div className='app'>
+      <Notification
+        time={notificationMilliseconds}
+        message={notificationText}
+      />
+      <GameBoard cardClicked={(id) => onCardClicked(id)} numCards={numCards} />
+      <Scoreboard score={10} hiScore={15} header={'Memory Card'} />
+    </div>
+  );
 }
 
 export default App;
